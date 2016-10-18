15c2d4c7d2e65080fbc809cea5c3245534bc634b
桂素伟 <axzxs2001@163.com>
2016-10-18 17:21:13 +0800

    添加HIS与医保DLL之间沟通的桥梁接口HisMedicalInsurance项目。

---
+++ ChinaMedicalInsurance/IHisMeicalInsurance/DynamicParameter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+
+
+namespace HisMeicalInsurance
+{
+    /// <summary>
+    /// 动态参数
+    /// </summary>
+    public class DynamicParameter : DynamicObject
+    {
+        Dictionary<string, dynamic> dic;
+        public DynamicParameter()
+        {
+            dic = new Dictionary<string, dynamic>();
+        }
+
+        /// <summary>
+        /// 获取值
+        /// </summary>
+        /// <param name="binder"></param>
+        /// <param name="result"></param>
+        /// <returns></returns>
+        public override bool TryGetMember(GetMemberBinder binder, out dynamic result)
+        {
+            var name = binder.Name;
+            if (dic.ContainsKey(name))
+            {
+                result = dic[name];
+            }
+            else
+            {
+                result = null;
+            }
+            return true;
+
+        }
+        /// <summary>
+        /// 赋值
+        /// </summary>
+        /// <param name="binder"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public override bool TrySetMember(SetMemberBinder binder, dynamic value)
+        {
+            var name = binder.Name;
+            if (dic.Keys.Contains(name))
+            {
+                dic[name] = value;
+            }
+            else
+            {
+                dic.Add(name, value);
+            }
+            return true;
+        }
+    }
+}
---
+++ ChinaMedicalInsurance/IHisMeicalInsurance/HisMeicalInsurance.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HisMeicalInsurance
+{
+    /// <summary>
+    /// HIS与医保DLL对接的接口，这是HIS与医保DLL之间的桥梁，本接口必需在HIS中实现（可用反射）
+    /// </summary>
+    public interface HisMeicalInsurance
+    {
+        /// <summary>
+        /// 调用接口方法
+        /// </summary> 
+        /// <param name="parmeter">HIS与医保DLL数据互通的桥梁，可自行协订HIS与医保DLL传递的参数</param>
+        /// <returns></returns>
+        short Invoke(dynamic parmeter);
+    }
+}
